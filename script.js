8107249b77a60260bbe939de0fcee9ead7e0ec04
Spencer Wood <spencer.wood@me.com>
2020-01-14 21:10:19 -0600

    wrote some basic structure comments to js file

---
+++ script.js
@@ -0,0 +1,13 @@
+//variables
+
+//user input on search to get weather
+
+//ajax to get connection to open weather
+
+//display forcast
+
+//store cities
+
+//add buttons to side
+
+//use local storage to get cities and store ones searched

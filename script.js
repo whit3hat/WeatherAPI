332fb1f848ca89860e14693a6663a25b688db75e
Spencer Wood <spencer.wood@me.com>
2020-01-15 23:28:08 -0600

    wrote ajax call and filled in info for query and apikey

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 //variables
-const apiKey = "";
-const url = "";
 //user input on search to get weather
-const city = "";
+const city = "kansas city";
+
+//API Variables
+const apiKey = "a22da7765e1c4b4916ddcc298644a47e";
+const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&mode=xml&appid=" + apiKey;
 //ajax to get connection to open weather
+$.ajax({
+    url:queryURL,
+    method: "GET"
+})
+//store all rretrieved data inside of an object 'response'
+.then(function(response){
+    //log the queryURL
+    console.log(response);
 
 //display forcast
 
 //store cities
-
+});
 //add buttons to side
 
 //use local storage to get cities and store ones searched

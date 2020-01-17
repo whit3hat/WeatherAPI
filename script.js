dec91a6160119442529232347ccc072bf5b18161
Spencer Wood <spencer.wood@me.com>
2020-01-17 17:24:21 -0600

    Changed first queryURL to get current information, added jquery to get current info

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //variables
 //user input on search to get weather
 
-const city = "Austin";
+const city = "Kansas City";
 
 //API Variables
 const apiKey = "a22da7765e1c4b4916ddcc298644a47e";
-const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&mode=json&appid=" + apiKey;
+const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&mode=json&appid=" + apiKey;
 //ajax to get connection to open weather
 $.ajax({
     url:queryURL,
@@ -15,8 +15,12 @@ $.ajax({
 .then(function(response){
     //log the queryURL
     console.log(response);
+//name , temp , windspeed , humidity
+$('.cityName').html('<h3>').html('<h3>' + response.name + '</h3>');
+$('.temp').html('<h6>').html('<h6>' + response.main.temp + '</h6>');
+$('.humidity').html('<h6>').html('<h6>' + response.main.humidity + '</h6>');
+$('.wind').html('<h6>').html('<h6>' + response.wind.speed + ' MPH</h6>');
 
-$('.cityName').html('<h3>').html('<h3>' + response.city.name + '</h3>');
 
 //display forcast : transfer content to HTML
 

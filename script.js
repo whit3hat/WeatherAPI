46df37c613d83ad6e53269cc13dd054bb862a657
Spencer Wood <spencer.wood@me.com>
2020-01-17 22:47:03 -0600

    Cleaned up JS file, created cities array to store previous searched cities, validated its still working and storing cities as expected in array

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//variables
-
-//user input on search to get weather
-//event handler for user clicking the search button
+//previously searched cities
+const cities = [];
 
 //API Variables
 function citySearch(city) {
@@ -20,18 +18,22 @@ function citySearch(city) {
 
     //name , temp , windspeed , humidity display forcast : transfer content to HTML
     $('.cityName').html('<h3>').html('<h3>' + response.name + '</h3>');
-    $('.temp').html('<h6>').html('<h6>' + response.main.temp + '</h6>');
+    $('.temp').html('<h6>').html('<h6>' + response.main.temp + ' F</h6>');
     $('.humidity').html('<h6>').html('<h6>' + response.main.humidity + ' %</h6>');
     $('.wind').html('<h6>').html('<h6>' + response.wind.speed + ' MPH</h6>');
     });
 }
 
+//event handler for user clicking the search button
 $('#button-addon1').on('click' , function(event) {
     //prevent the button from trying to submit the form
     event.preventDefault();
     //store city name
     const city = $('.form-control').val().trim();
+    cities.push(city);
     citySearch(city);
+    //logging Cities Array storing previous searches
+    console.log(cities);
 });
 //store cities
 

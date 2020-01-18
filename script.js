5e7d1b1868d913c67b7c5f23fdaeb6578b404ceb
Spencer Wood <spencer.wood@me.com>
2020-01-17 22:29:08 -0600

    disabled slim version of JQuery at bottom of HTML, it was causing issues using ajax call

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 //variables
-//user input on search to get weather
 
-const city = "Kansas City";
+//user input on search to get weather
+//event handler for user clicking the search button
 
 //API Variables
-const apiKey = "a22da7765e1c4b4916ddcc298644a47e";
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&mode=json&appid=" + apiKey;
-//ajax to get connection to open weather
-$.ajax({
-    url:queryURL,
-    method: "GET"
-})
-//store all rretrieved data inside of an object 'response'
-.then(function(response){
-    //log the queryURL
+function citySearch(city) {
+    const apiKey = "a22da7765e1c4b4916ddcc298644a47e";
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&mode=json&appid=" + apiKey;
+
+    //ajax to get connection to open weather
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    })
+    .then(function(response){
+
+      //log the queryURL
     console.log(response);
-//name , temp , windspeed , humidity
-$('.cityName').html('<h3>').html('<h3>' + response.name + '</h3>');
-$('.temp').html('<h6>').html('<h6>' + response.main.temp + '</h6>');
-$('.humidity').html('<h6>').html('<h6>' + response.main.humidity + ' %</h6>');
-$('.wind').html('<h6>').html('<h6>' + response.wind.speed + ' MPH</h6>');
 
+    //name , temp , windspeed , humidity display forcast : transfer content to HTML
+    $('.cityName').html('<h3>').html('<h3>' + response.name + '</h3>');
+    $('.temp').html('<h6>').html('<h6>' + response.main.temp + '</h6>');
+    $('.humidity').html('<h6>').html('<h6>' + response.main.humidity + ' %</h6>');
+    $('.wind').html('<h6>').html('<h6>' + response.wind.speed + ' MPH</h6>');
+    });
+}
 
-//display forcast : transfer content to HTML
-
-//store cities
+$('#button-addon1').on('click' , function(event) {
+    //prevent the button from trying to submit the form
+    event.preventDefault();
+    //store city name
+    const city = 'austin';
+    citySearch(city);
 });
+//store cities
+
 //add buttons to side
 
 //use local storage to get cities and store ones searched

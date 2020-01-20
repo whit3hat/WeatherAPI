f117a1d85766b871cd4b28b909022d2ff3d0f488
Spencer Wood <spencer.wood@me.com>
2020-01-20 16:08:07 -0600

    Created for loop for the array of hours for 5 days forecast

--- script.js
+++ script.js
@@ -16,24 +16,43 @@ function citySearch(city) {
       //log the queryURL
     console.log(response);
 
-    //name , temp , windspeed , humidity display forcast : transfer content to HTML
+    //name , temp , windspeed , humidity display forecast : transfer content to HTML
     $('.cityName').html('<h3>').html('<h3>' + response.name + '</h3>');
     $('.temp').html('<h6>').html('<h6>' + response.main.temp + ' F</h6>');
     $('.humidity').html('<h6>').html('<h6>' + response.main.humidity + ' %</h6>');
     $('.wind').html('<h6>').html('<h6>' + response.wind.speed + ' MPH</h6>');
     
     });
+
     //add buttons to side
 
-    //call for the 5 day forcast data 
+
+    //call for the 5 day forecast data 
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&units=imperial&mode=json&appid=" + apiKey,
         method: "GET"
     })
-    .then (function (forcast){
-        //log the forcast data
-        console.log(forcast);
-    });
+    .then (function (forecast){
+        //log the forecast data
+        console.log(forecast);
+    
+
+    //array to store city forecast for 5 day
+    cityForecast = [];
+
+    //for loop to get the days forecast from the reponse above
+    for(let i = 0; i < forecast.list.length; i+=8){
+        //temp value to store the forecast data
+        const tempForecast = new cityForecast(forecast.list[i].dt_txt,
+            forecast.list[i].weather[0].icon,
+            forecast.list[i].main.temp_max,
+            forecast.list[i].main.temp_min)
+        
+        cityForecast.push(tempForecast);
+    }
+        //console log of the array for the 5 day
+        console.log(cityForecast);
+});       
 };
 
 //event handler for user clicking the search button
@@ -43,11 +62,12 @@ $('#button-addon1').on('click' , function(event) {
     //store city name
     const city = $('.form-control').val().trim();
     //store cities
-    cities.push(city);
+    // cities.push(city);
     citySearch(city);
     //logging Cities Array storing previous searches
     console.log(cities);
 });
+
 
 
 
